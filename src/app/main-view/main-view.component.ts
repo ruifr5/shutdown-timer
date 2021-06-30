f87bc844ec83c8326e52f03c427850d8bafc7308
Rui Franco <ruifr5@hotmail.com>
2021-06-30 05:49:35 +0100

    added shortcuts

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { ChildProcessService } from 'ngx-childprocess';
 
 @Component({
@@ -13,6 +13,84 @@ export class MainViewComponent implements OnInit {
   constructor(private _childProcessService: ChildProcessService) {}
 
   ngOnInit() {}
+
+  @HostListener('window:keydown', ['$event'])
+  keyEvent({ key, ctrlKey, altKey }: KeyboardEvent) {
+    if (this.runSpecialShortcut(key, ctrlKey || altKey)) {
+      return;
+    }
+
+    const keyNumber = key === ' ' ? 0 : +key;
+
+    if (keyNumber >= 0 && keyNumber <= 9) {
+      if (ctrlKey || altKey) {
+        this.hours = this.hours * 10 + keyNumber;
+      } else {
+        this.minutes = this.minutes * 10 + keyNumber;
+      }
+
+      if (this.minutes > 100) {
+        const numberToTransferToHours = this.leftmostDigit(this.minutes);
+        this.minutes -= numberToTransferToHours * 100;
+        this.hours = this.hours * 10 + numberToTransferToHours;
+      }
+
+      if (this.hours > 100) {
+        const numberToRemoveFromHours = this.leftmostDigit(this.hours);
+        this.hours -= numberToRemoveFromHours * 100;
+      }
+    }
+    this.correctTimeIndicators();
+  }
+
+  // returns true if shortcut existed, false if nothing happens
+  runSpecialShortcut(key: string, isModified: boolean): boolean {
+    switch (key.toLowerCase()) {
+      case 'f1':
+        // 2h preset
+        this.shutdown(60 * 60 * 2);
+        return true;
+      case 'f2':
+        // 3h preset
+        this.shutdown(60 * 60 * 3);
+        return true;
+      case 'delete':
+      case 'backspace':
+      case 'c':
+        this.minutes = this.hours = 0;
+        return true;
+      case 'escape':
+        this.close();
+        return true;
+      case 'enter':
+      case 's':
+        this.shutdownFromSelected();
+        return true;
+      case 'a':
+        this.abort();
+        return true;
+      case '+':
+        isModified ? this.incHours() : this.incMinutes();
+        return true;
+      case '-':
+        isModified ? this.decHours() : this.decMinutes();
+        return true;
+    }
+    return false;
+  }
+
+  leftmostDigit(number: number) {
+    return Math.floor(number / 100);
+  }
+
+  correctTimeIndicators() {
+    if (this.hours > 23) {
+      this.hours = 23;
+    }
+    if (this.minutes > 59) {
+      this.minutes = 59;
+    }
+  }
 
   incHours() {
     this.hours = this.hours === 23 ? 0 : this.hours + 1;
